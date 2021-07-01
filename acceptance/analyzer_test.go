be89cd83a2c80cce86f498856e35d0c92b7eb48a
Juan Bustamante <bustamantejj@gmail.com>
2021-07-01 16:22:44 -0500

    Making the test changes more readable
    Co-authored-by: Natalie Arellano <narellano@vmware.com>

--- acceptance/analyzer_test.go
+++ acceptance/analyzer_test.go
@@ -206,10 +206,15 @@ func testAnalyzerFunc(platformAPI string) func(t *testing.T, when spec.G, it spe
 				h.SkipIf(t, runtime.GOOS == "windows", "Not relevant on Windows")
 				output := h.DockerRun(t,
 					analyzeImage,
-					h.WithFlags("--env", "CNB_PLATFORM_API="+platformAPI,
-						"--network", registryNetwork),
-					h.WithBash(fmt.Sprintf("chown -R 9999:9999 /layers; chmod -R 775 /layers; %s %s; ls -al /layers", analyzerPath,
-						noAuthRegistry.RepoName("some-image"))),
+					h.WithFlags(
+						"--env", "CNB_PLATFORM_API="+platformAPI,
+						"--network", registryNetwork,
+					),
+					h.WithBash(
+						fmt.Sprintf("chown -R 9999:9999 /layers; chmod -R 775 /layers; %s %s; ls -al /layers",
+							analyzerPath,
+							noAuthRegistry.RepoName("some-image")),
+					),
 				)
 
 				h.AssertMatch(t, output, "2222 3333 .+ \\.")
